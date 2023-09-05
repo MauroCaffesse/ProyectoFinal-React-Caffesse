f6bd4d8dd71d113267ea2ace612ee5d08682b43c
Mauro Caffesse <mscaffesse@gmail.com>
2023-09-05 17:18:51 -0300

    componente Contador creado

---
+++ src/Components/ItemCount/ItemCount.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+  const [quantity, setQuantity] = useState(initial);
+
+  const increment = () => {
+    if (quantity < stock) {
+      setQuantity(quantity + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex items-center">
+        <button onClick={decrement} className="p-1 rounded-full">
+          -
+        </button>
+        <h4 className="mx-5">{quantity}</h4>
+        <button onClick={increment} className="p-1 rounded-full">
+          +
+        </button>
+      </div>
+      <div>
+        <button onClick={() => onAdd(quantity)} disabled={!stock}>
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ItemCount;
