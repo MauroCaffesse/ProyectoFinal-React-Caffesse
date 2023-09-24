b096c765933d500e0916ee9a79cf2bbb28fdcb2a
Mauro Caffesse <mscaffesse@gmail.com>
2023-09-24 18:43:40 -0300

    implementacion librerias para proyecto final

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import ItemListContainer from "./Components/ItemListContainer/ItemListContainer"
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetailContainer";
+import Cart from "./Components/Cart/Cart";
+import Checkout from "./Components/Checkout/Checkout";
 import { CartProvider } from "./context/CartContext";
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
               element={<ItemListContainer />}
             />
             <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
             <Route path="*" element={<h1>404 NOT FOUND</h1>} />
           </Routes>
         </CartProvider>
--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -1,10 +1,19 @@
 import { ShoppingCartIcon } from "@heroicons/react/24/solid";
+import { useCart } from "../../context/CartContext";
+import { useNavigate } from "react-router-dom";
 
 const CartWidget = () => {
+  const { totalQuantity } = useCart();
+  const navigate = useNavigate();
+
   return (
-    <div className="flex justify-center items-center">
-      <ShoppingCartIcon className="w-6 h-6 m-2" />0
-    </div>
+    <button
+      onClick={() => navigate("/cart")}
+      className="flex justify-center items-center"
+    >
+      <ShoppingCartIcon className="w-6 h-6 m-2" />
+      {totalQuantity}
+    </button>
   );
 };
 
