13383cbcb08aae6dcf1e0293c35a7e191004fc7e
Mauro Caffesse <mscaffesse@gmail.com>
2023-09-24 18:44:44 -0300

    modificacion itemdetail componente

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -1,13 +1,13 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import ItemCount from "../ItemCount/ItemCount";
 import { Link } from "react-router-dom";
 
-import { CartContext } from "../../context/CartContext";
+import { useCart } from "../../context/CartContext";
 
-const ItemDetail = ({ id, name, image, description, price }) => {
+const ItemDetail = ({ id, name, image, description, stock, price }) => {
   const [quantityAdded, setQuantityAdded] = useState(0);
 
-  const { addItem } = useContext(CartContext);
+  const { addItem } = useCart();
 
   const handleQuantity = (quantity) => {
     setQuantityAdded(quantity);
@@ -24,20 +24,22 @@ const ItemDetail = ({ id, name, image, description, price }) => {
   return (
     <article className="flex flex-col justify-center items-center">
       <header className="m-2">
-        <h2>{name}</h2>
+        <h2 className="m-1 text-2xl">{name}</h2>
       </header>
       <picture className="mb-2">
         <img src={image} alt={name} className="w-80 rounded-xl" />
       </picture>
-      <section>
-        <p className="mb-2">Price: ${price}</p>
-        <p className="mb-2">Description: {description}</p>
+      <section className="flex flex-col items-center">
+        <p className="mb-2 border-b-2 w-30 font-bold">Price: ${price}</p>
+        <p className="mb-2 border-b-2">Description: {description}</p>
       </section>
       <footer>
         {quantityAdded > 0 ? (
-          <Link to="/cart">Terminar compra</Link>
+          <Link to="/cart">Complete purchase</Link>
+        ) : stock > 0 ? (
+          <ItemCount initial={1} stock={stock} onAdd={handleQuantity} />
         ) : (
-          <ItemCount initial={1} stock={10} onAdd={handleQuantity} />
+          <h3 className="m-4 text-lg font-semibold">Out of stock</h3>
         )}
       </footer>
     </article>
